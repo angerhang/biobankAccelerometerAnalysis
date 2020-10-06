5e84e1aaf7a2178591d57200b5f2036fdac63221
angerhang <514140809@qq.com>
2020-10-06 23:44:47 +0100

    update processing

--- processRaineGen1.py
+++ processRaineGen1.py
@@ -1,9 +1,10 @@
 from accelerometer import accUtils
-accUtils.writeStudyAccProcessCmds("/well/doherty/users/cxx579/project_data/raine/gen1_26/gt3x/wrist/", 
+accUtils.writeStudyAccProcessCmds("/well/doherty/users/cxx579/project_data/raine/gen1_26/gt3x/wrist/",
                                   cmdsFile="process-raine-gen1-cmds.txt",
-                                  outDir="/well/doherty/users/cxx579/project_data/raine/gen1_26/gt3x/wrist/baseline_oct2",
+                                  outDir="/well/doherty/users/cxx579/project_data/raine/gen1_26/gt3x/wrist/baseline_oct6",
                                   accExt="gt3x",
                                   cmdOptions="--deleteIntermediateFiles False "+
+                                             "--timeZone Australia/Perth "+
                                              "--useFilter False "+
                                              "--npyOutput True "+
                                              "--rawOutput True "+
--- processRaineGen2.py
+++ processRaineGen2.py
@@ -1,9 +1,10 @@
 from accelerometer import accUtils
-accUtils.writeStudyAccProcessCmds("/well/doherty/users/cxx579/project_data/raine/gen2_22/gt3x/wrist/", 
+accUtils.writeStudyAccProcessCmds("/well/doherty/users/cxx579/project_data/raine/gen2_22/gt3x/wrist/",
                                   cmdsFile="process-raine-gen2-cmds.txt",
                                   outDir="/well/doherty/users/cxx579/project_data/raine/gen2_22/gt3x/wrist/baseline_oct2",
                                   accExt="gt3x",
                                   cmdOptions="--deleteIntermediateFiles False "+
+                                             "--timeZone Australia/Perth "+
                                              "--useFilter False "+
                                              "--npyOutput True "+
                                              "--rawOutput True "+
