26ac5f3568df88bec812deee8256a28294517de4
angerhang <514140809@qq.com>
2021-01-23 18:52:50 +0000

    update model name

--- utilities/downloadDataModelsWindows.sh
+++ utilities/downloadDataModelsWindows.sh
@@ -13,7 +13,7 @@ fi
 # delete and newly download activity model files
 curl -L ${downloadDir}doherty-may20.tar --output activityModels/doherty-may20.tar
 curl -L ${downloadDir}willetts-may20.tar --output activityModels/willetts-may20.tar
-curl -L ${downloadDir}old-walmsley-nov20.tar --output activityModels/walmsley-nov20.tar
+curl -L ${downloadDir}walmsley-nov20.tar --output activityModels/walmsley-nov20.tar
 
 # download sample training file
 #if ! [ -f "activityModels/labelled-acc-epochs.csv" ]
